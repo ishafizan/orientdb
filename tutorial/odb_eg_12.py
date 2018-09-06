744d8a82637298cced35dbb76e60d85682578942
Ishafizan Ishak <ishafizan@gmail.com>
2018-09-06 20:12:53 +0800

    Add files via upload

---
+++ tutorial/odb_eg_12.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from json import dumps, loads
+from time import sleep, time
+
+try:
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(BASE_DIR)
+    from util import util_gen
+    from util import util_log
+    from util import util_odb
+except ImportError:
+    raise Exception("import util files failed")
+
+# -----------------------------
+__author__ = 'Ishafizan'
+__date__ = "2018-09-06"
+# -----------------------------
+
+log = util_log.logger()
+start = time()
+log.info("start: %s" % util_gen.getcurrdt())
+log.info("*-" * 40)
+
+# --------------------------------------
+# QUERY via rest
+# --------------------------------------
+
+log.info("query: Find top 3 Hotels that have been booked most times")
+query_str = "SELECT Name, Type, in(\"HasStayed\").size() AS NumberOfBookings " \
+            "FROM Hotels " \
+            "ORDER BY NumberOfBookings DESC LIMIT 3"
+
+log.info("---*" * 20)
+res = util_odb.post_command(log, query_str)
+log.info(dumps(res["result"]))
+
+# -- END
+end = time()
+elapsed_time = end - start
+log.info("*-" * 40)
+log.info("time taken: %ss" % elapsed_time)
+log.info("FINISH")
